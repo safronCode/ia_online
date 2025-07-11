d9c5167f3c34b8d859195f23e7a0654aabc1d27d
safronCode <justfugue@gmail.com>
2025-07-11 17:49:14 +0300

    Подправил редирект. Если пользователь попадает на .../deals/ или другой похожий путь, то вместо отображения лобби по этому пути его редиректит на .../lobby/ и уже там отображает шаблон

--- lobby/views/reload_lobby.py
+++ lobby/views/reload_lobby.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.conf import settings
+from django.shortcuts import render, redirect
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
 
 
 @main_auth(on_cookies=True)
 def reload_lobby(request):
-    #app_settings = settings.APP_SETTINGS
-    return render(request, 'start_page.html')
+    if request.path == '/lobby/':
+        return render(request, 'start_page.html')
+    return redirect('/lobby/')
